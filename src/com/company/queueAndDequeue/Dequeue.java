089c82f401e0e8fbd8021ab1c6d31a698d5eb8ba
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-02-26 16:42:09 +0300

    update Dequeue

--- src/com/company/queueAndDequeue/Dequeue.java
+++ src/com/company/queueAndDequeue/Dequeue.java
@@ -2,48 +2,20 @@ package com.company.queueAndDequeue;
 
 import java.util.LinkedList;
 
-public class Dequeue<T> extends AbstractDequeue<T> {
-    private int size;
-    private final LinkedList<T> storage;
-    private int popFirstStatus;
-    private int peekFirstStatus;
+public class Dequeue<T> extends ParentQueue<T> {
     private int popLastStatus;
     private int peekLastStatus;
 
     public Dequeue() {
-        this.size = NIL;
-        this.storage = new LinkedList<>();
-        this.popFirstStatus = NIL;
-        this.peekFirstStatus = NIL;
         this.popLastStatus = NIL;
         this.peekLastStatus = NIL;
     }
 
-
-    @Override
     public void pushFirst(T value) {
         storage.addFirst(value);
         size++;
     }
 
-    @Override
-    public void pushLast(T value) {
-        storage.addLast(value);
-        size++;
-    }
-
-    @Override
-    public void popFirst() {
-        if (size > 0) {
-            storage.removeFirst();
-            size--;
-            popFirstStatus = OK;
-        } else {
-            popFirstStatus = ERR;
-        }
-    }
-
-    @Override
     public void popLast() {
         if (size > 0) {
             storage.removeLast();
@@ -54,20 +26,6 @@ public class Dequeue<T> extends AbstractDequeue<T> {
         }
     }
 
-    @Override
-    public T peekFirst() {
-        T value;
-        if (size > 0) {
-            value = storage.getFirst();
-            peekFirstStatus = OK;
-        } else {
-            value = null;
-            peekFirstStatus = ERR;
-        }
-        return value;
-    }
-
-    @Override
     public T peekLast() {
         T value;
         if (size > 0) {
@@ -80,27 +38,10 @@ public class Dequeue<T> extends AbstractDequeue<T> {
         return value;
     }
 
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public int getPopFirstStatus() {
-        return popFirstStatus;
-    }
-
-    @Override
     public int getPopLastStatus() {
         return popLastStatus;
     }
 
-    @Override
-    public int getPeekFirstStatus() {
-        return peekFirstStatus;
-    }
-
-    @Override
     public int getPeekLastStatus() {
         return peekLastStatus;
     }
