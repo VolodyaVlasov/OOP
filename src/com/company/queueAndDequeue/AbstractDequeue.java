97eaa329f47373769dacbdfaff46fd05d982c87d
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-02-26 16:34:06 +0300

    update AbstractDequeue

--- src/com/company/queueAndDequeue/AbstractDequeue.java
+++ src/com/company/queueAndDequeue/AbstractDequeue.java
@@ -1,12 +1,6 @@
 package com.company.queueAndDequeue;
 
 public abstract class AbstractDequeue<T> extends AbstractParentQueue<T> {
-    public static final int POP_LAST_OK = OK;  // последняя popLast отработала нормально
-    public static final int POP_LAST_ERR = ERR; // очередь пустая
-
-    public static final int PEEK_LAST_OK = OK;  // последняя peekLast вернула корректное значение
-    public static final int PEEK_LAST_ERR = ERR; // очередь пустая
-
     //команды:
 
     // постусловие: элемент добавлен в начало очереди
@@ -23,6 +17,5 @@ public abstract class AbstractDequeue<T> extends AbstractParentQueue<T> {
 
     // дополнительные запросы
     public abstract int getPopLastStatus();
-
     public abstract int getPeekLastStatus();
 }
