1c417ba61ccc2881cf75002e07bc3c6e847dfc16
VolodyaVlasov <ivlasovvladimir@gmail.com>
2021-02-28 16:36:43 +0300

    added PowerSet

--- src/com/company/PowerSet.java
+++ src/com/company/PowerSet.java
@@ -1,38 +1,55 @@
 package com.company;
 
+import javax.sql.PooledConnection;
+
 public class PowerSet extends AbstractPowerSet {
     private int length;
     private Node[] storage;
     private int size;
     private int addStatus;
     private int removeStatus;
+    private int intersectionStatus;
+    private int unionStatus;
+    private int differenceStatus;
+    private int isSubsetStatus;
+
+    public PowerSet() {
+        this(16);
+    }
+
+    public PowerSet(int length) {
+        size = 0;
+        this.length = length;
+        storage = new Node[length];
+        addStatus = 0;
+        removeStatus = 0;
+        intersectionStatus = 0;
+        unionStatus = 0;
+        differenceStatus = 0;
+        isSubsetStatus = 0;
+    }
 
 
     @Override
     public void add(String value) {
         int index = findBasket(value);
-        Node start = storage[index];
-        Node prev = null;
-        while (start != null) {
-            if (start.value.equals(value)) {
-                break;
-            }
-            prev = start;
-            start = start.next;
-        }
-        if (start == null) {
-            start = new Node(value);
-            if (prev != null) {
-                prev = start;
-            }
-            size++;
-            addStatus = OK;
-            if (size > length) {
-                increaseArray();
-            }
+        if (storage[index] == null) {
+            storage[index] = new Node(value);
         } else {
-            addStatus = ERR;
+            Node start = storage[index];
+            Node prev = null;
+            while (start != null) {
+                if (start.value.equals(value)) {
+                    addStatus = ERR;
+                    return;
+                }
+                prev = start;
+                start = start.next;
+            }
+            prev.next = new Node(value);
         }
+        size++;
+        addStatus = OK;
     }
 
     @Override
@@ -63,70 +80,162 @@ public class PowerSet extends AbstractPowerSet {
     }
 
     @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
     public boolean check(String value) {
-       int index = findBasket(value);
-       Node start = storage[index];
-       while (start != null) {
-           if (start.value.equals(value)) {
-               return true;
-           }
-           start = start.next;
-       }
-       return false;
+        int index = findBasket(value);
+        Node start = storage[index];
+        while (start != null) {
+            if (start.value.equals(value)) {
+                return true;
+            }
+            start = start.next;
+        }
+        return false;
     }
 
     @Override
-    public AbstractPowerSet intersection(AbstractPowerSet set) {
-        return null;
+    public PowerSet intersection(PowerSet set) {
+        PowerSet powerSet = new PowerSet(size);
+        if (size == 0 || set.size() == 0) {
+            intersectionStatus = ERR;
+        } else {
+            for (int i = 0; i < length; i++) {
+                Node start = storage[i];
+                while (start != null) {
+                    if (set.check(start.value)) {
+                        powerSet.add(start.value);
+                    }
+                    start = start.next;
+                }
+            }
+            intersectionStatus = OK;
+        }
+        return powerSet;
     }
 
     @Override
-    public AbstractPowerSet union(AbstractPowerSet set) {
-        return null;
+    public PowerSet union(PowerSet set) {
+        PowerSet powerSet = new PowerSet(size + set.size);
+        if (size == 0 || set.size() == 0) {
+            unionStatus = ERR;
+        } else {
+            for (int i = 0; i < length; i++) {
+                Node start = storage[i];
+                while (start != null) {
+                    powerSet.add(start.value);
+                    start = start.next;
+                }
+            }
+            for (String s : set.toArray()) {
+                powerSet.add(s);
+            }
+            unionStatus = OK;
+        }
+        return powerSet;
     }
 
     @Override
-    public AbstractPowerSet difference(AbstractPowerSet set) {
-        return null;
+    public PowerSet difference(PowerSet set) {
+        PowerSet powerSet = new PowerSet();
+        if (size == 0 || set.size() == 0) {
+            differenceStatus = ERR;
+        } else {
+            for (int i = 0; i < length; i++) {
+                Node start = storage[i];
+                while (start != null) {
+                    if (!set.check(start.value)) {
+                        powerSet.add(start.value);
+                    }
+                    start = start.next;
+                }
+            }
+            differenceStatus = OK;
+        }
+        return powerSet;
     }
 
     @Override
-    public AbstractPowerSet isSubset(AbstractPowerSet set) {
-        return null;
+    public boolean isSubset(PowerSet set) {
+        boolean answer = true;
+        if (size == 0 || set.size() == 0) {
+            isSubsetStatus = ERR;
+            answer = false;
+        } else {
+            for (int i = 0; i < length; i++) {
+                Node start = storage[i];
+                while (start != null) {
+                    if (!set.check(start.value)) {
+                        answer = false;
+                        break;
+                    }
+                    start = start.next;
+                }
+            }
+            isSubsetStatus = OK;
+        }
+        return answer;
     }
 
     @Override
     public int getAddStatus() {
-        return 0;
+        return addStatus;
     }
 
     @Override
     public int getRemoveStatus() {
-        return 0;
+        return removeStatus;
     }
 
     @Override
     public int getIntersectionStatus() {
-        return 0;
+        return intersectionStatus;
     }
 
     @Override
     public int getUnionStatus() {
-        return 0;
+        return unionStatus;
     }
 
     @Override
     public int getDifferenceStatus() {
-        return 0;
+        return differenceStatus;
     }
 
     @Override
     public int getIsSubsetStatus() {
-        return 0;
+        return isSubsetStatus;
     }
 
     public int findBasket(String value) {
         return value.hashCode() % length;
+    }
+
+    private void increaseArray() {
+        length = length * 3 / 2 + 1;
+        Node[] temp = storage;
+        storage = new Node[length];
+        for (Node node : temp) {
+            while (node != null) {
+                add(node.value);
+                node = node.next;
+            }
+        }
+    }
+
+    public String[] toArray() {
+        String[] array = new String[size];
+        int index = 0;
+        for (Node node : storage) {
+            while (node != null) {
+                array[index++] = node.value;
+                node = node.next;
+            }
+        }
+        return array;
     }
 
     private static class Node {
